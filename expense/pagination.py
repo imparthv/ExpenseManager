db8b57ef5fd9bfeb087504f4fd7b50c420b76701
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-03 19:00:14 +0530

    fix: Fixed number of items per page

--- expense/pagination.py
+++ expense/pagination.py
@@ -4,4 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 class ExpensePagination(PageNumberPagination):
     page_size = 2
     max_page_size = 100
-    page_query_param = 'page_size'
+    page_size_query_param = 'page_size' # Number of items per page
