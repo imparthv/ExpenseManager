95bb7f9c4d9170a9b63a33e7b7597122533c575d
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-03 15:10:26 +0530

    feat: Added serializers to accounts app.

---
+++ accounts/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+# Creating a serializer to validate user , hash password and store user
+class RegisterSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
+        return user
