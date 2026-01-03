4ae47f9c85d0df31df77b210bfdd90f8af191b0d
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-03 15:42:08 +0530

    feat: Registered expense model

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Expenses
 
-# Register your models here.
+@admin.register(Expenses)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'category', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('description','user__username')
