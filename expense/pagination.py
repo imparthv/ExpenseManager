74fcdfe008f4daa2af4397b8fe40d9c5cf6c55dd
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-03 18:49:03 +0530

    feat: Custom pagination class to implement pagination

---
+++ expense/pagination.py
@@ -0,0 +1,7 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class ExpensePagination(PageNumberPagination):
+    page_size = 2
+    max_page_size = 100
+    page_query_param = 'page_size'
