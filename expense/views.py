477d0deba3f117db75a3d63453af984608e97db5
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-03 16:14:57 +0530

    feat:Added views to handle functionalities to view expenses

--- expense/views.py
+++ expense/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import ExpenseSerializer
+from .models import Expenses
+
+
+class ExpenseListCreateView(generics.ListCreateAPIView):
+    serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Providing user level isolation 
+    def get_queryset(self):
+        return Expenses.objects.filter(user=self.request.user)
+
+    # Perform operations for logged in user only
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
+
+    
+    def get_queryset(self):
+        return Expenses.objects.filter(user=self.request.user)
+
+        
