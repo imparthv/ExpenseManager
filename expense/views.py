3657757b5c65bb94ae24c99016cbf2c6f3f34221
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-07 15:31:22 +0530

    feat: Added views for monthly and categorywise expenses

--- expense/views.py
+++ expense/views.py
@@ -12,6 +12,15 @@ from .filters import ExpenseFilter
 
 from .pagination import ExpensePagination
 
+# importing timezone
+from django.utils import timezone
+
+# Importing sum class to be used along with aggregation
+from django.db.models import Sum
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 # View to list user expenses
 # Only authenticated user/Logged in user can view their expenses
 # Providing custom filter functionality to filter based on date and category
@@ -48,5 +57,44 @@ class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
         return Expenses.objects.filter(user=self.request.user)
     
 
+def fetch_dates():
+    today = timezone.now()
+    date_dict = {
+        "today": today,
+        "month_start": today.replace(day=1)
+    }
+
+    return date_dict
+
+# View to find total expense made by the user in given date range
+# Uses aggregate instead of python loops
+class MonthlyExpenseSummaryView(APIView):
+    def get(self, request):
+       
+        date_dict = fetch_dates()
+
+        # Calculating total expense for a given date range
+        expense_result = Expenses.objects.filter(user= request.user, 
+                                                 created_at__gte = date_dict.get("month_start"), created_at__lte = date_dict.get("today")).aggregate(total_expense = Sum('amount'))
+
+        # Returning response based in month , year and expense
+        return Response({
+            "month": date_dict.get("today").strftime("%B"),
+            "year": date_dict.get("today").strftime("%Y"),
+            "total_expense": expense_result.get("total_expense")
+        })
+    
+# View for category-wise expense
+class CategoryExpenseSummaryView(APIView):
+    def get(self, request):
+        date_dict = fetch_dates()
+
+         # Calculating total expense for a given date range
+        categorywise_expense= Expenses.objects.filter(user= request.user, 
+                                                 created_at__gte = date_dict.get("month_start"), created_at__lte = date_dict.get("today")).values("category").annotate(total_expense = Sum('amount'))
+        
+        return Response({
+            categorywise_expense
+        })
 
         
