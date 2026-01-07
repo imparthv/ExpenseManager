4848a7730637cd088cbec8c79503536cfbd54fe5
Parth Vasdewani <imparthvasdewani@gmail.com>
2026-01-07 15:30:08 +0530

    feat: Modified pre-existing urls and added new urls for monthly and categorywise expenses

--- expense/urls.py
+++ expense/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import ExpenseListCreateView, ExpenseDetailView
+from .views import ExpenseListCreateView, ExpenseDetailView, MonthlyExpenseSummaryView, CategoryExpenseSummaryView
 
 urlpatterns = [
-    path('expense/', ExpenseListCreateView.as_view(), name='expense-list'),
-    path('expense/<int:pk>/', ExpenseDetailView.as_view(), name='expense-detail')
+    path('', ExpenseListCreateView.as_view(), name='expense-list'),
+    path('<int:pk>/', ExpenseDetailView.as_view(), name='expense-detail'),
+    path('dashboard/monthly-summary/', MonthlyExpenseSummaryView.as_view(), name='monthly-expense-summary'),
+    path('dashboard/category-summary/', CategoryExpenseSummaryView.as_view(), name='category-expense-summary'),
 ]
